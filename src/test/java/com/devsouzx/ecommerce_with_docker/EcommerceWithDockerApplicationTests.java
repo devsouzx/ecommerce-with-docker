5b3eded2e38bee13ec478ec4b46cf131d0484c68
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-24 18:24:43 -0300

    first commit

---
+++ src/test/java/com/devsouzx/ecommerce_with_docker/EcommerceWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsouzx.ecommerce_with_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
